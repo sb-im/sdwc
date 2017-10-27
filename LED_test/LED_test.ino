cf204cf165175636905dab3b13c0c896cf770738
YongXin Shi <shiyongxin@aliyun.com>
2017-10-27 10:12:15 +0800

    socket ctl

--- LED_test/LED_test.ino
+++ LED_test/LED_test.ino
@@ -1,6 +1,9 @@
 int LED = 7;
 String comchar;
 
+//char line[500] = "";   // 传入的串行数据
+int ret = 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -10,10 +13,26 @@ void setup() {
 }
 
 void loop() {
+  char line[500] = "";
+  if (Serial.available() > 0) {
+    // 读取传入的数据:  读到 \n 为止，或者最多 500 个字符
+    ret = Serial.readBytesUntil('\n', line, 500);
+
+    //打印你得到的：
+    Serial.print("I received: ");
+    Serial.println(line);
+  }
+  // 每1秒做一个输出
+  /*
+  delay(1000);
+  Serial.println("I am waiting! ");
+  
   // 从串口缓存区读取全部数据到一个字符串型变量
   comchar = Serial.readString();  
   Serial.print("Serial.read: ");  
   Serial.println(comchar);
+  */
+  comchar = line;
   if (comchar == "start") {
     digitalWrite(LED, HIGH);   // 点亮LED
 
@@ -25,4 +44,5 @@ void loop() {
     delay(1000);               // 持续1秒
   }
   delay(1000);
+
 }
