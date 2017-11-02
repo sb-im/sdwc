4d488abde09ec6ce18d6923959e191ec08230836
YongXin Shi <shiyongxin@aliyun.com>
2017-11-02 10:23:19 +0800

    simplify code

--- action/action.ino
+++ action/action.ino
@@ -1,35 +1,12 @@
 /*
  * SuperDock
  *
- * Version: v0.10
- * Date: 2017.10.27
+ * Version: v0.11
+ * Date: 2017.11.2
  *
  */
-/*
-#define BUTTON_START A0
-#define BUTTON_DOOR_OPENED A1
-#define BUTTON_PLAT_TOP A2
-#define BUTTON_LANDED A3
-#define BUTTON_FIXED A4
-                                                        // define motor
-#define MOTOR_DOOR_EN 13
-#define MOTOR_DOOR_DIR 12
-#define MOTOR_DOOR_CLK 11
 
-#define MOTOR_PLAT_EN 10
-#define MOTOR_PLAT_DIR 9
-#define MOTOR_PLAT_CLK 8
-
-#define MOTOR_FIX_EN 7
-#define MOTOR_FIX_DIR 6
-#define MOTOR_FIX_CLK 5
-*/
-
-//#define ACTION_MAX_SIZE 3
-
-//int LED = 7;
 String comchar;
-
 
 struct Action {
   String  cmd;
@@ -41,7 +18,6 @@ struct Action {
   int  clk;
   int  speed;
 };
-//action1, action2;
 
 /*
 *命令，限位开关接口，限位开关状态， cw方向， en, cw, clk
@@ -50,7 +26,7 @@ struct Action {
 struct Action action_lib[] = {
   {"plet_up", A0, HIGH, HIGH, 10, 9, 8, 1000},
   {"plet_down", A0, HIGH, LOW, 10, 9, 8, 100},
-  {"plet_down", A0, HIGH, LOW, 10, 9, 8, 100},
+  {"plet_do", A0, HIGH, LOW, 10, 9, 8, 100},
   {"stopplet_down", A2, HIGH, HIGH, 13, 12, 11, 500}
 };
 
@@ -69,30 +45,7 @@ int init(struct Action *action) {
 
 void setup() {
   Serial.begin(9600);
-/*
-  // 定义7为输出引脚
-  pinMode(LED, OUTPUT);
 
-
-  pinMode(BUTTON_START, INPUT_PULLUP);
-  pinMode(BUTTON_DOOR_OPENED, INPUT_PULLUP);
-  pinMode(BUTTON_PLAT_TOP, INPUT_PULLUP);
-  pinMode(BUTTON_LANDED, INPUT_PULLUP);
-  pinMode(BUTTON_FIXED, INPUT_PULLUP);
-
-  // define motor output
-  pinMode(MOTOR_DOOR_EN, OUTPUT);
-  pinMode(MOTOR_DOOR_DIR, OUTPUT);
-  pinMode(MOTOR_DOOR_CLK, OUTPUT);
-
-  pinMode(MOTOR_PLAT_EN, OUTPUT);
-  pinMode(MOTOR_PLAT_DIR, OUTPUT);
-  pinMode(MOTOR_PLAT_CLK, OUTPUT);
-
-  pinMode(MOTOR_FIX_EN, OUTPUT);
-  pinMode(MOTOR_FIX_DIR, OUTPUT);
-  pinMode(MOTOR_FIX_CLK, OUTPUT);
-*/
   for( int a = 0; a < sizeof(action_lib)/sizeof(action_lib[0]); a++ ) {
     init(&action_lib[a]);
   }
@@ -133,27 +86,10 @@ void loop() {
   comchar = msg();
   //for( int a = 0; a < ACTION_MAX_SIZE; a++ ) {
   for( int a = 0; a < sizeof(action_lib)/sizeof(action_lib[0]); a++ ) {
-    Serial.println(a);
+    //Serial.println(a);
     if (comchar == action_lib[a].cmd) {
       action(&action_lib[a]);
     }
   }
 
-/*
-//测试用代码
-  if (comchar == "start") {
-    digitalWrite(LED, HIGH);   // 点亮LED
-    //Serial.println(action3.button);
-    //Serial.println(action3.button_status);
-    action( &action_lib[0] );
-
-    //delay(1000);               // 持续1秒
-  } else if (comchar == "stop") {
-
-    digitalWrite(LED, LOW);    // 熄灭LED
-
-    //delay(1000);               // 持续1秒
-  }
- // delay(1000);
-*/
 }
