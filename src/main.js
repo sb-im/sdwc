2ca9cb8503ab4f46abdb61bd380ecd04a18839e8
YongXin Shi <shiyongxin@aliyun.com>
2018-01-10 14:13:30 +0800

    add Login.vue

--- src/main.js
+++ src/main.js
@@ -8,10 +8,18 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import App from './App.vue'
+import App2 from './Login.vue'
 
 Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+
+new Vue ({
+  el: '#app2',
+  render: h => h(App2)
+
 })
