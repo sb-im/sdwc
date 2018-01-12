4a2a0bf7fb8eb6fdc0a682168125224eda4422ea
YongXin Shi <shiyongxin@aliyun.com>
2018-01-12 16:48:40 +0800

    login demo

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+  <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
     <el-form-item label="账号" prop="user">
-      <el-input type="password" v-model="ruleForm2.user" auto-complete="off"></el-input>
+      <el-input type="password" v-model="ruleForm.username" auto-complete="off"></el-input>
     </el-form-item>
     <el-form-item label="密码" prop="pass">
-      <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+      <el-input type="password" v-model="ruleForm.password" auto-complete="off"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="login">提交</el-button>
@@ -19,8 +19,8 @@
         if (value === '') {
           callback(new Error('请输入账号'));
         } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
           }
           callback();
         }
@@ -29,20 +29,20 @@
         if (value === '') {
           callback(new Error('请输入密码'));
         } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
           }
           callback();
         }
       };
       return {
-        ruleForm2: {
-          user: '',
-          pass: '',
+        ruleForm: {
+          username: '',
+          password: '',
           checkPass: '',
           age: ''
         },
-        rules2: {
+        rules: {
           user: [
             { validator: validateUser, trigger: 'blur' }
           ],
@@ -66,14 +66,26 @@
 
         //this.update()
 
-        this.$http.get(api_url)
+        //this.$http.get(api_url)
+        //this.$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+        //this.$http.defaults.headers.post['Access-Control-Request-Method'] = 'POST'
+        //this.$http.defaults.headers.post['Access-Control-Request-Headers'] = 'X-PINGOTHER'
+/*
+        this.$http.create({
+          "Access-Control-Request-Method": "POST",
+          "Access-Control-Request-Headers": "X-PINGOTHER"
+        })
+        */
+        this.$http.get(api_url + "/" + this.ruleForm.username + "/" + this.ruleForm.password)
         .then((response) => {
           console.log(this)
           //this.update()
           console.log(response.data)
           //this.$store.state.config.token = response.data.token
           this.$store.commit("token", response.data)
-          this.$router.push('app')
+          if (response.data.token) {
+            this.$router.push('app')
+          }
         })
         .catch((error) => {
           console.log(error)
