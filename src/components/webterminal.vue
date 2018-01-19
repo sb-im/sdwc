d0234e7c62c5aa0b3aee2b5d103aeb678f0fe02c
YongXin Shi <shiyongxin@aliyun.com>
2018-01-19 19:21:11 +0800

    add command.json

--- src/components/webterminal.vue
+++ src/components/webterminal.vue
@@ -2,7 +2,6 @@
   <fieldset>
     <legend>操作控制</legend>
     <div align="right">
-      <el-button @click="" type="primary" plain>获取Items信息</el-button>
       <el-button @click="send" type="warning" plain>初始化</el-button>
       <el-button @click="send" type="danger" plain>停止</el-button>
     </div>
@@ -66,29 +65,12 @@
 </template>
 
 <script>
+import Command from './command.json'
 
   export default {
     data() {
       return {
-        commands: [
-          { name: '打开舱门～ | ω・´) ～', value: 'door_up' },
-          { name: '关闭舱门～ (＾o＾)ﾉ ～', value: 'door_down' },
-          { name: '平台上升～ (°ー°〃) ～', value: 'plat_up' },
-          { name: '平台下降～ ( ;´д`) ～', value: 'plat_down' },
-          { name: '固定开始～ (・∀・) ～', value: 'fix_up' },
-          { name: '固定解除～ (*´д`) ～', value: 'fix_down' },
-          { name: '电池开锁～ (*ﾟーﾟ) ～', value: 'lock_up' },
-          { name: '电池锁定～ (・∀・) ～', value: 'lock_down' },
-          { name: '电池前进～ (╬ﾟдﾟ) ～', value: 'push_up' },
-          { name: '电池后退～ (￣∇￣) ～', value: 'push_down' },
-          { name: '电池上升～ (>д<) ～', value: 'battery_up' },
-          { name: '电池下降～(((　ﾟдﾟ)))～', value: 'battery_down' },
-          { name: '顺时对准～ ( ☉д⊙) ～', value: 'roate_up' },
-          { name: '顺时离开～(￣皿￣)～', value: 'roate_down' },
-          { name: '逆时离开～ 唔喵 ～', value: 're_roate_down' },
-          { name: '全部停止～ Σ( ﾟдﾟ) ～', value: 'stop_all' },
-          { name: '测试 ～ (〃∀〃) ～', value: 'test' }
-        ],
+        commands: Command,
         hostname: location.hostname,
         port: '22333',
         message: 'hello',
