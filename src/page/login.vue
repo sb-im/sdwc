c00e61a71fc504b2ccb64490bface463e8f2cad6
YongXin Shi <shiyongxin@aliyun.com>
2018-01-22 20:16:09 +0800

    response Login

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="background">
-    <div class="login">
+    <el-row :gutter="10" type="flex" justify="center">
+      <el-col :xs="22" :sm="14" :md="12" :lg="8" :xl="6" class="login-bg">
+        <el-row :gutter="10" type="flex" justify="center">
+          <el-col :xs="24" :sm="22" :md="22" :lg="18" :xl="18">
 
-      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="账号" prop="username">
-          <el-input type="password" v-model="ruleForm.username" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input type="password" v-model="ruleForm.password" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="login">提交</el-button>
-        </el-form-item>
-      </el-form>
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+              <el-form-item label="账号" prop="username">
+                <el-input type="text" v-model="ruleForm.username" auto-complete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input type="password" v-model="ruleForm.password" auto-complete="off"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="login">提交</el-button>
+              </el-form-item>
+            </el-form>
 
-    </div>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -88,11 +94,11 @@
   }
 </script>
 <style scoped>
-
-  .login {
-    background-color: #fff9;
-    padding: 8% 5%;
-    margin: 10% 35%;
+  .login-bg {
+    border-radius: 4px;
+    background: #fff9;
+    margin: 200px 0px;
+    padding: 120px 0px;
   }
 
   .background {
