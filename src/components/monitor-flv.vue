cd19de717613e119e35da295a1a5507db5f839fa
YongXin Shi <shiyongxin@aliyun.com>
2018-01-27 12:26:50 +0800

    add flv demo

---
+++ src/components/monitor-flv.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+  <video ref="vvv" id="videoElement"></video>
+            <el-button @click="play" type="danger" plain>play</el-button>
+  </div>
+</template>
+
+
+
+<script>
+import flvjs from 'flv.js'
+//this.prototype.$flv = flvjs
+
+  export default {
+    data() {
+      return {}
+    },
+    props: {
+      source: {
+        type: String,
+        default: 'vertical'
+      }
+    },
+    methods: {
+      play() {
+        if (flvjs.isSupported()) {
+          console.log(this)
+          //var videoElement = document.getElementById('videoElement');
+          var videoElement = this.$refs.vvv
+          var flvPlayer = flvjs.createPlayer({
+            type: 'flv',
+            url: 'http://127.0.0.1:7001/live/movie.flv'
+          });
+          flvPlayer.attachMediaElement(videoElement);
+          flvPlayer.load();
+          flvPlayer.play();
+        }
+      }
+    }
+
+  }
+
+
+</script>
--- src/contents/air.vue
+++ src/contents/air.vue
@@ -2,12 +2,14 @@
   <div>
     <div v-for="video in node.video">
       <monitor-img :source="video"></monitor-img>
+      <monitor-flv :source="video"></monitor-flv>
     </div>
     <webterminal :websocket=node.websocket :commands=command></webterminal>
   </div>
 </template>
 <script>
 import MonitorImg from '../components/monitor-img.vue'
+import MonitorFlv from '../components/monitor-flv.vue'
 import WebTerminal from '../components/webterminal.vue'
 import Command from '../components/air-command.json'
 
@@ -26,6 +28,7 @@ import Command from '../components/air-command.json'
     },
     components: {
       'monitor-img': MonitorImg,
+      'monitor-flv': MonitorFlv,
       'webterminal': WebTerminal
     }
   }
