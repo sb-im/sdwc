c9c36c595721193cd5d1c9e69c90bdefbdfaa440
metal A-wing <shiyongxin@aliyun.com>
2018-03-15 14:15:29 +0800

    Reduce sidemenu.vue

--- src/page/sidemenu.vue
+++ src/page/sidemenu.vue
@@ -1,46 +1,43 @@
 <template>
-  <div>
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @select="handleSelect"
-      @open="handleOpen"
-      @close="handleClose"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b">
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    @select="handleSelect"
+    @open="handleOpen"
+    @close="handleClose"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
 
-      <el-submenu index="air">
-        <template slot="title">
-          <i class="el-icon-tickets"></i>
-          <span>{{ $t('sidemenu.air') }}</span>
-        </template>
-        <div v-for="item in items" v-if="item.type_name == 'air'">
-          <el-menu-item :index="String(item.id)">{{ item.name }}</el-menu-item>
-        </div >
-      </el-submenu>
-      <el-submenu index="depot">
-        <template slot="title">
-          <i class="el-icon-setting"></i>
-          <span>{{ $t('sidemenu.depot') }}</span>
-        </template>
+    <el-submenu index="air">
+      <template slot="title">
+        <i class="el-icon-tickets"></i>
+        <span>{{ $t('sidemenu.air') }}</span>
+      </template>
+      <div v-for="item in items" v-if="item.type_name == 'air'">
+        <el-menu-item :index="String(item.id)">{{ item.name }}</el-menu-item>
+      </div >
+    </el-submenu>
+    <el-submenu index="depot">
+      <template slot="title">
+        <i class="el-icon-setting"></i>
+        <span>{{ $t('sidemenu.depot') }}</span>
+      </template>
 
-        <div v-for="item in items" v-if="item.type_name == 'depot'">
-          <el-menu-item :index="String(item.id)">{{ item.name }}</el-menu-item>
-        </div >
-      </el-submenu>
+      <div v-for="item in items" v-if="item.type_name == 'depot'">
+        <el-menu-item :index="String(item.id)">{{ item.name }}</el-menu-item>
+      </div >
+    </el-submenu>
 
-      <el-menu-item index="log">
-        <i class="el-icon-menu"></i>
-        <span slot="title">{{ $t('sidemenu.log') }}</span>
-      </el-menu-item>
+    <el-menu-item index="log">
+      <i class="el-icon-menu"></i>
+      <span slot="title">{{ $t('sidemenu.log') }}</span>
+    </el-menu-item>
 
-    </el-menu>
-  </div>
+  </el-menu>
 </template>
 
 <script>
-
   export default {
     data() {
       return {
@@ -48,7 +45,7 @@
         items: []
       }
     },
-    created: function () {
+    created() {
       this.getItems()
     },
     methods: {
@@ -57,11 +54,9 @@
         //console.log(key, keyPath)
         for (let item of this.items) {
           if (keyPath[1] == item.id) {
-            //console.log(item.id)
             this.$store.commit("linkadd", item)
           }
         }
-        //this.$store.commit("linkadd", response.data)
       },
       handleOpen(key, keyPath) {
         //console.log(key, keyPath);
@@ -71,28 +66,11 @@
       },
       getItems() {
         //console.log("getItems")
-        //let api_url = "/node_config.json"
-        let api_url = this.$store.state.config.server + "/nodes" + this.$store.state.config.suffix
-        //console.log(api_url)
+        let api_url = this.$store.state.api.nodes
         this.$http.get(api_url)
         .then((response) => {
-          //console.log(response.data)
           this.items = response.data
           this.$store.commit("items", response.data)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-      },
-      getNodeConfig: function () {
-        console.log(this.$store.state.config.token)
-
-        //console.log(location.host)
-        //let api_url = "/node_config.json"
-        let api_url = this.$store.state.config.server + "/nodes"
-        this.$http.get(api_url)
-        .then((response) => {
-          this.items = response.data
         })
         .catch((error) => {
           console.log(error)
@@ -102,4 +80,3 @@
     }
   }
 </script>
-
