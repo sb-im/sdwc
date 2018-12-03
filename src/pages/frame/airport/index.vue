b4ebb9d9f455844daf986d0187ee975f1c08bfb4
lxb134230 <1342303258@qq.com>
2018-12-03 18:48:24 +0800

    Style fine-tuning

--- src/pages/frame/airport/index.vue
+++ src/pages/frame/airport/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="wrapper">
     <el-row class="head">
-      <el-col :span="10" class="monitor">
+      <el-col class="monitor">
         <el-header class="header font-24">
           <img src="../../../assets/images/airport/a_monitor.svg"/>实时监控
         </el-header>
@@ -9,7 +9,7 @@
 
         </section>
       </el-col>
-      <el-col :span="14" class="other">
+      <el-col class="other">
         <section class="weather">
           <el-header class="header font-24">
             <img src="../../../assets/images/airport/a_weather.svg"/>室外天气
@@ -59,11 +59,11 @@
       </el-header>
       <el-row>
         <el-col :span="12" class="control-left">
-          <el-col :span="6" class="battery text-c">
+          <el-col :span="5" class="battery text-c">
             <img src="../../../assets/images/airport/a_battery.svg"/>
             <p>A仓电池</p>
           </el-col>
-          <el-col :span="18" class="infos" tag="ul">
+          <el-col :span="19" class="infos" tag="ul">
             <li class="d-f">
               <div class="item-label">循环次数</div>
               <div class="item-text">
@@ -96,11 +96,11 @@
           </el-col>
         </el-col>
         <el-col :span="12">
-          <el-col :span="6" class="battery text-c">
+          <el-col :span="5" class="battery text-c">
             <img src="../../../assets/images/airport/a_battery.svg"/>
             <p>B仓电池</p>
           </el-col>
-          <el-col :span="18" class="infos" tag="ul">
+          <el-col :span="19" class="infos" tag="ul">
             <li class="d-f">
               <div class="item-label">循环次数</div>
               <div class="item-text">
@@ -138,7 +138,7 @@
       <el-header class="header font-24">
         <img src="../../../assets/images/airport/a_h_control.svg"/>高级控制
       </el-header>
-      <el-row class="high-content">
+      <el-row type="flex" class="high-content">
         <el-col tag="ul" class="status">
           <li class="font-18">状态：</li>
           <li>
@@ -150,7 +150,7 @@
             <el-button class="font-16" type="primary">无人机状态</el-button>
           </li>
         </el-col>
-        <el-col class="operate" tag="table">
+        <el-col tag="table" class="operate">
           <tr>
             <td class="font-18">电机运动</td>
             <td class="text-c"><img src="../../../assets/images/airport/a_airport.svg"/></td>
@@ -181,7 +181,23 @@
           </tr>
         </el-col>
       </el-row>
-      <!--<section class=""></section>-->
+      <section class="logs">
+        <header class="header font-18">日志：</header>
+        <ul class="logs-content">
+          <li>AUTO> Mode AUTO</li>
+        </ul>
+      </section>
+      <section class="debug">
+        <p>以下命令仅限开发人员调试使用</p>
+        <el-button class="font-16" type="danger">平台升顶</el-button>
+        <el-button class="font-16" type="danger">平台降底</el-button>
+        <el-button class="font-16" type="danger">平台举平</el-button>
+        <div class="btns f-r">
+          <el-input class="f-l inp"></el-input>
+          <el-button class="f-l font-16" type="danger">发送</el-button>
+          <el-button class="f-l font-16">清空</el-button>
+        </div>
+      </section>
     </section>
   </section>
 </template>
@@ -191,10 +207,7 @@
   export default {
     data() {
       return {
-        activeIndex:'1',
-        tableTest:[{
-          id:'A007'
-        }]
+        activeIndex:'1'
       }
     },
     components: {
@@ -207,11 +220,6 @@
 </script>
 
 <style scoped>
-  .wrapper {
-    margin: 8px;
-    min-height: calc(100% - 18px);
-    border: 1px solid #e4eaef;
-  }
   .head {
     width: 100%;
     height: 520px;
@@ -219,6 +227,7 @@
   }
   .header {
     line-height: 60px;
+    padding: 0 10px;
     border-bottom: 1px solid #e4eaef;
   }
   .header img {
@@ -227,34 +236,31 @@
     padding-right: 10px;
   }
   .monitor {
+    width: 640px;
     border-right: 1px solid #e4eaef;
   }
+  .monitor .header{padding: 0 20px;}
   .other {
+    width: calc(100% - 640px);
     height: 100%;
   }
   .weather {
     padding: 0 15px;
     border-bottom: 1px solid #e4eaef;
   }
-  .infos {
-    padding: 0 15px;
-  }
+  .infos {padding: 0 15px;}
   .video {
     width: 100%;
     height: 460px;
     background-color: #eee;
   }
-  .weather .w-info {max-width: 520px;}
   .weather .w-info .status {
     color: #42a847;
-    font-size: 70px;
+    font-size: 68px;
     margin: 15px 0;
   }
-  .weather .w-info .items {
-    width: 400px;
-    margin: 10px 0;
-  }
-  .weather .w-info .items p {width: 33.3333%;}
+  .weather .w-info .items {margin: 10px 0;}
+  .weather .w-info .items p {width: calc(100% / 3);}
   .weather .w-info .chart {margin-top: 20px;}
   .weather .w-info .chart-view {
     width: 360px;
@@ -263,39 +269,30 @@
     background-color: #eee;
   }
   .infos .info-tab{width: 100%;}
-  .infos .info-tab td {padding: 10px 15px;}
+  .infos .info-tab td {
+    white-space: nowrap;
+    padding: 10px 0;
+    text-align: center;
+  }
   .infos .info-tab td.id-num {width: 10%;}
 
   .control {
     padding: 0 15px;
     border-bottom: 1px solid #e4eaef;
   }
-  .control .battery {
-    padding: 20px 0;
-  }
-  .control .infos {
-    padding: 16px 0;
-  }
-  .control .control-left {
-    border-right: 1px solid #e4eaef;
-  }
+  .control .battery {padding: 20px 0;}
+  .control .infos {padding: 16px 0;}
+  .control .control-left {border-right: 1px solid #e4eaef;}
   .control .battery img{
     width: 75px;
     height: 75px;
     margin-bottom: 10px;
   }
-  .control .item-label {
-    width: 80px;
-  }
-  .control .item-text {
-    padding: 0 15px;
-    min-height: 30px;
-  }
-  .control .item-text span:last-child {
-    padding-left: 5px;
-  }
+  .control .item-label {width: 80px;}
+  .control .item-text {min-height: 30px;}
+  .control .item-text span:last-child {padding-left: 5px;}
   .control .item-text img {
-    left: 10px;
+    left: 5px;
     top: 50%;
     width: 40px;
     height: 40px;
@@ -307,11 +304,9 @@
   }
   .control .item-text.pos-r {
     height: 45px;
-    padding-left: 75px;
+    padding-left: 64px;
   }
-  .control .item-text.debug {
-    margin-top: 20px;
-  }
+  .control .item-text.debug {margin-top: 20px;}
   .control .item-text.debug:before {
     content: '';
     position: absolute;
@@ -325,10 +320,8 @@
     width: 290px;
     height: 20px;
   }
-
-  .control.high .high-content {
-    padding-top: 15px;
-  }
+  .control.high {padding-bottom: 15px;border-bottom: 0;}
+  .control.high .high-content {padding-top: 15px;}
   .control.high .status {
     width: 200px;
     border-right: 1px solid #e4eaef;
@@ -346,13 +339,12 @@
   }
   .control.high .operate {
     width: calc(100% - 200px);
-
+    margin-left: 20px;
   }
   .control.high .status li:first-child,
   .control.high .operate tr:first-child td{padding-bottom: 10px;}
   .control.high .operate td {
     width: calc(100% / 6);
-    padding: 0 15px;
   }
   .control.high .operate td img {
     width: 40px;
@@ -372,4 +364,49 @@
   }
   .control.high .operate .btns td:first-child button{margin: 0;}
   .control.high .operate .btns td:last-child button {margin: 0 auto;}
+
+  .logs > .header {
+    border-bottom: 0;
+    line-height: 46px;
+    padding: 0;
+  }
+  .logs-content {
+    width: 100%;
+    height: 300px;
+    padding: 5px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    border: 1px solid #e4eaef;
+    overflow-y: auto;
+  }
+  .logs-content li {
+    line-height: 18px;
+    color: #999;
+  }
+  .debug > p {
+    position: relative;
+    color: #999;
+    margin-top: 10px;
+    font-size: 14px;
+    line-height: 26px;
+  }
+  .debug > p:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    right: 0;
+    height: 1px;
+    width: 81%;
+    -webkit-transform: translateY(-50%);
+    -moz-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    -o-transform: translateY(-50%);
+    transform: translateY(-50%);
+    background-color: #e4eaef
+  }
+  .debug .btns > .inp{
+    width: auto;
+    margin-right: 10px;
+  }
 </style>
