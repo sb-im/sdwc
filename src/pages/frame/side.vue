713989c0f0a8a1a773473b4f76c7e5ec01f814d6
lxb134230 <1342303258@qq.com>
2018-12-06 18:57:11 +0800

    Side menu further improved functions

--- src/pages/frame/side.vue
+++ src/pages/frame/side.vue
@@ -1,22 +1,41 @@
 <template>
   <el-aside class="aside" width="160px">
     <el-menu
-      :default-active="activeIndex"
+      :default-active="$store.state.active"
       class="menu"
       @select="menuSelect"
       @open="menuOpen"
-      @close="menuClose"
       background-color="#545c64"
       text-color="#ccc">
-      <el-menu-item index="task" class="font-18">
-        <img src="../../assets/images/aside/task.svg"/>任务管理
+      <el-menu-item index="task" class="menu-item">
+        <img src="../../assets/images/aside/task.svg"/><span class="font-18">任务管理</span>
       </el-menu-item>
-      <el-menu-item index="airport" class="font-18">
-        <img src="../../assets/images/aside/airport.svg"/>机场
-      </el-menu-item>
-      <el-menu-item index="drone" class="font-18">
-        <img src="../../assets/images/aside/drone.svg"/>无人机
-      </el-menu-item>
+      <el-submenu index="air">
+        <template slot="title">
+          <img src="../../assets/images/aside/airport.svg"/><span class="font-18">机场</span>
+        </template>
+        <el-menu-item
+          class="menu-item"
+          v-for="(val,index) in nodes"
+          :key="index"
+          v-if="val.type_name==='air'"
+          :index="val.id+''">
+          {{ val.name }}
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="depot">
+        <template slot="title">
+          <img src="../../assets/images/aside/drone.svg"/><span class="font-18">无人机</span>
+        </template>
+        <el-menu-item
+          class="menu-item"
+          v-for="(val,index) in nodes"
+          :key="index"
+          v-if="val.type_name==='depot'"
+          :index="val.id+''">
+          {{ val.name }}
+        </el-menu-item>
+      </el-submenu>
     </el-menu>
   </el-aside>
 </template>
@@ -25,23 +44,48 @@
   export default {
     data() {
       return {
-        activeIndex: 'task'
+
       }
     },
-    created(){
-      this.activeIndex = this.$store.state.aside?this.$store.state.aside:'task';
-      this.$store.commit('asideLink',this.activeIndex)
+    props: {
+      nodes: {
+        type: Array,
+        required: true,
+        default: () => {
+          return [];
+        }
+      }
+    },
+    mounted(){
+
     },
     methods: {
-      menuSelect(key,keyPath){
-        this.$store.commit('asideLink',key);
-        console.log(key, keyPath);
+      menuSelect(key) {
+        if(key!=='task'){
+          for(let item of this.nodes) {
+            key === (item.id+'') && this.$store.commit('linkAdd',item);
+          }
+        }else this.$store.commit('tabChange',key);
       },
-      menuOpen(key,keyPath){
-        console.log(key,keyPath);
+      menuOpen(key) {
+        switch (key) {
+          case 'air':this.openFirst('air');break;
+          case 'depot':this.openFirst('depot');break;
+        }
       },
-      menuClose(key,keyPath){
-        console.log(key,keyPath);
+      /**
+       * 菜单打开时(此项菜单下无子项打开)，默认打开此项菜单下的第一个子项
+       * @param type   类型(air/depot)，须与nodes.json里面的type_name匹配
+       */
+      openFirst(type) {
+        for (let item of this.nodes) {
+          if (item.type_name===type) {
+            this.$store.state.links.findIndex(val => {
+              return (+val.id) === (+item.id);
+            }) === -1 && this.$store.commit('linkAdd',item);
+            return true;
+          }
+        }
       }
     }
   }
@@ -61,17 +105,18 @@
     height: 30px;
     padding-right: 10px;
   }
-  .aside .menu .el-menu-item {
+  .aside .menu .menu-item {
+    min-width: 0;
     border-top: 1px solid transparent;
     border-bottom: 1px solid transparent;
   }
-  .aside .menu .is-active {
+  .aside .menu .menu-item.is-active {
     position: relative;
     color: #fff;
     border-color: #fff5;
     box-shadow: inset 4px 0 0 0 #28b3e4;
   }
-  .aside .menu .is-active:after {
+  .aside .menu .menu-item.is-active:after {
     content: '';
     position: absolute;
     top: 50%;
