db23226e2682ef2e007682edad0931293dda2566
lxb134230 <1342303258@qq.com>
2018-12-06 18:58:22 +0800

    Tabs components linked to the left menu

--- src/pages/frame/tabs.vue
+++ src/pages/frame/tabs.vue
@@ -1,24 +1,22 @@
 <template>
-  <el-tabs class="tabs" v-model="activeIndex" type="card" closable @tab-click="handleClick">
-    <el-tab-pane class="tab-item" label="用户管理" name="first">
-      <sd-task v-if="this.$store.state.aside==='task'" v-cloak></sd-task>
-      <sd-airport v-if="this.$store.state.aside==='airport'" v-cloak></sd-airport>
-      <sd-drone v-if="this.$store.state.aside==='drone'" v-cloak></sd-drone>
+  <el-tabs
+    class="tabs"
+    v-model="$store.state.active"
+    type="card"
+    @tab-remove="tabRemove"
+    @tab-click="tabsClick">
+    <el-tab-pane class="tab-item" label="任务管理" name="task">
+      <sd-task></sd-task>
     </el-tab-pane>
-    <el-tab-pane class="tab-item" label="配置管理" name="second">
-      <sd-task v-if="this.$store.state.aside==='task'" v-cloak></sd-task>
-      <sd-airport v-if="this.$store.state.aside==='airport'" v-cloak></sd-airport>
-      <sd-drone v-if="this.$store.state.aside==='drone'" v-cloak></sd-drone>
-    </el-tab-pane>
-    <el-tab-pane class="tab-item" label="角色管理" name="third">
-      <sd-task v-if="this.$store.state.aside==='task'" v-cloak></sd-task>
-      <sd-airport v-if="this.$store.state.aside==='airport'" v-cloak></sd-airport>
-      <sd-drone v-if="this.$store.state.aside==='drone'" v-cloak></sd-drone>
-    </el-tab-pane>
-    <el-tab-pane class="tab-item" label="定时任务补偿" name="fourth">
-      <sd-task v-if="this.$store.state.aside==='task'" v-cloak></sd-task>
-      <sd-airport v-if="this.$store.state.aside==='airport'" v-cloak></sd-airport>
-      <sd-drone v-if="this.$store.state.aside==='drone'" v-cloak></sd-drone>
+    <el-tab-pane
+      v-for="(val,index) in $store.state.links"
+      :key="index"
+      :label="val.name"
+      class="tab-item"
+      closable
+      :name="val.id+''">
+      <sd-air v-if="val.type_name==='air'" v-cloak></sd-air>
+      <sd-drone v-if="val.type_name==='depot'" v-cloak></sd-drone>
     </el-tab-pane>
   </el-tabs>
 </template>
@@ -27,29 +25,47 @@
   import Task from './task'
   import Airport from './airport'
   import Drone from './drone'
+
   export default {
     data() {
       return {
-        activeIndex: 'first'
+
       }
+    },
+    props:{
+      /*nodes:{
+        type:Array
+      }*/
     },
     created(){
 
     },
     components: {
-      'sd-task':Task,
-      'sd-airport':Airport,
-      'sd-drone':Drone
+      'sd-task': Task,
+      'sd-air': Airport,
+      'sd-drone': Drone
     },
     methods: {
-      handleClick(key,keyPath){
-        console.log(key, keyPath);
-      },
-      menuOpen(key,keyPath){
+      tabsClick(key,keyPath) {
         console.log(key,keyPath);
       },
-      menuClose(key,keyPath){
-        console.log(key,keyPath);
+      tabRemove(id) {
+        this.$store.commit('tabChange',this.tabActive(id,this.$store.state.active,this.$store.state.links));
+        this.$store.commit("linkDel", +id);
+      },
+      tabActive(del,cur,links){
+        if(links.length !== 1) {
+          if(cur === del) {
+            links.forEach((tab, index) => {
+              if(tab.id === (+del)){
+                let next = links[index+1] || links[index-1];
+                cur = next ? next.id : cur;
+                return true;
+              }
+            });
+          }
+        }else cur = 'task';
+        return cur;
       }
     }
   }
@@ -58,9 +74,7 @@
 <style>
   .tabs {height: 100%;}
   .tabs .el-tabs__header {margin: 0;}
-  .tabs .el-tabs__content {
-    height: calc(100% - 41px);
-  }
+  .tabs .el-tabs__content {height: calc(100% - 41px);}
   .tabs .tab-item {
     padding: 5px;
     height: 100%;
