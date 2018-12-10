c0d1ce3aaa5a9276850f9330b838c178a50a2745
lxb134230 <1342303258@qq.com>
2018-12-10 18:21:12 +0800

    Add plans & nodes with features

--- src/config/store.js
+++ src/config/store.js
@@ -10,12 +10,15 @@ export default new Vuex.Store({
     api: API,
     config: Config,
     token: '',
-    items: [],
+    nodes: [],
+    plans: [],
     links: [],
-    active:'task',
-    taskPage:'view'
+    active: '',
+    planInfo: {},
+    planLogs: [],
+    planPage:'view'
   },
-  mutations: {
+    mutations: {
     config(state, arg) {
       let keys = Object.keys(state.api);
       state.config = arg.config;
@@ -32,30 +35,73 @@ export default new Vuex.Store({
         state.config.suffix = state.config.suffix + config.token
       }
     },
-    items(state, items) {
-      state.items = items
-    },
-    linkAdd(state, item) {
-      if (state.links.length === 0) {
-        state.links.push(item);
-      } else {
-        // 先判断是否存在然后再添加
-        state.links.findIndex(val => {
-          return (+val.id) === (+item.id);
-        }) === -1 && state.links.push(item);
+    itemAdd(state, arg) {
+      switch (arg.type) {
+        case 'nodes':state.nodes = arg.data;break;
+        case 'plans':state.plans = arg.data;break;
       }
-      // 激活对应tabs
-      state.active = item.id+'';
     },
-    tabChange(state, item){
-      state.active = (item.id?item.id:item)+'';
+    linkAdd(state, arg) {
+      if (state.links.length === 0) {
+        state.links.push(arg);
+      } else {
+        // 先判断类型，若不存在对应类型则直接添加，若存在则继续判断是否有相同id存在，不存在则添加
+        state.links.findIndex(val => val.type === arg.type) === -1 ?
+          state.links.push(arg) :
+            (state.links.findIndex(val => {
+                return val.type === arg.type && (+val.item.id) === (+arg.item.id);
+              }) === -1 && state.links.push(arg));
+      }
+      // 激活对应Tab
+      state.active = arg.type + arg.item.id;
     },
-    linkDel(state, id) {
-      // state.links = state.links.filter(tab => tab.id !== item_id)
-      state.links = state.links.filter(tab => tab.id !== id);
+    tabChange(state, item) {
+      state.active = item;
     },
-    taskLink(state, name){
-      state.taskPage = name;
+    linkDel(state, obj) {
+      state.links.forEach((val,index) => {
+        if (val.type === obj.type && val.item.id === (+obj.id)) {
+          state.links.splice(index,1);
+          return true;
+        }
+      });
+    },
+    planLink(state, name){
+      state.planPage = name;
+    },
+    planInfo(state, info) {
+      state.planInfo = info;
+    },
+    planLogs(state, logs) {
+      state.planLogs = logs;
+    }
+  },
+  actions: {
+    // 获取任务信息
+    getPlanInfo(context,arg) {
+      context.commit('planLink','view');// 先将任务界面切回'查看任务'
+      arg._this.$http.get(`${context.state.api.plans}/${arg.id}`)
+        .then(res => {
+          if (res.status === 200) {
+            context.commit('planInfo',res.data);
+            context.dispatch('getPlanLogs',{_this: arg._this, id: res.data.id});
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    // 获取任务历史日志
+    getPlanLogs(context,arg) {
+      arg._this.$http.get(`${context.state.api.plans}/${arg.id}/plan_logs`)
+        .then(res => {
+          if (res.status === 200) {
+            res.status===200 && context.commit('planLogs',res.data);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
-})
+});
