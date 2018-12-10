4b75539101c258563c325af08fd8efd4c8efacaa
lxb134230 <1342303258@qq.com>
2018-12-10 18:12:30 +0800

    Add monitor-flash component

---
+++ src/components/rt-monitor/monitor-flash.vue
@@ -0,0 +1,83 @@
+<template>
+
+  <!--<video :src="source"></video>-->
+  <video-player class="vjs-custom-skin" ref="videoPlayer" :options="playerOptions" @timeupdate="onTimeupdate">
+  </video-player>
+  <!--customEventName="customstatechangedeventname"
+
+    @play="onPlayerPlay($event)"
+    @pause="onPlayerPause($event)"
+    @ended="onPlayerEnded($event)"
+    @waiting="onPlayerWaiting($event)"
+    @playing="onPlayerPlaying($event)"
+    @loadeddata="onPlayerLoadeddata($event)"
+    @timeupdate="onPlayerTimeupdate($event)"
+    @canplay="onPlayerCanplay($event)"
+    @canplaythrough="onPlayerCanplaythrough($event)"
+
+    @statechanged="playerStateChanged($event)"
+    @ready="playerReadied"-->
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        playerOptions: {
+          autoplay: false,
+          controls: true,
+          techOrder: ['flash'],
+          flash: { hls: { withCredentials: false } },
+          html5: { hls: { withCredentials: false } },
+          live: true,
+          autoplay: true,
+          sources: [{
+            type: 'rtmp/mp4',
+            src: 'rtmp://api.sb.im:1935/live/outdoor'
+          }],
+          poster: '/src/assets/images/header/logo.png'
+        }
+      }
+    },
+    props: {
+      source: {
+        type: String,
+        required: true,
+        default: ''
+      }
+    },
+    created() {
+
+    },
+    components:{
+      // 'video-player':videoPlayer
+    },
+    methods: {
+      // record current time
+      onTimeupdate(e) {
+        console.log('currentTime', e.cache_.currentTime)
+      },
+      // listen event
+      onPlayerPlay(player) {
+        // console.log('player play!', player)
+      },
+      onPlayerPause(player) {
+        // console.log('player pause!', player)
+      },
+      // ...player event
+
+      // or listen state event
+      playerStateChanged(playerCurrentState) {
+        // console.log('player current update state', playerCurrentState)
+      },
+
+      // player is ready
+      playerReadied(player) {
+        console.log('the player is readied', player)
+        // you can use it to do something...
+        // player.[methods]
+      }
+    }
+  }
+</script>
