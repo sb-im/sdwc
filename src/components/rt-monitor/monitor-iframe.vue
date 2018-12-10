0c23df4513785df0c263f7086aa3a8001865ced4
lxb134230 <1342303258@qq.com>
2018-12-10 18:12:53 +0800

    Add monitor-iframe component

---
+++ src/components/rt-monitor/monitor-iframe.vue
@@ -0,0 +1,26 @@
+<template>
+  <iframe
+    width="100%"
+    height="100%"
+    frameborder="0"
+    allowfullscreen="true"
+    webkitallowfullscreen="true"
+    mozallowfullscreen="true"
+    :src="source"></iframe>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {}
+    },
+    props: {
+      source: {
+        type: String,
+        required: true,
+        default: ''
+      }
+    }
+  }
+</script>
