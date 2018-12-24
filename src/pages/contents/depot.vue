ec75c1fffb16270558128e042dc10c6ffd48e701
lxb134230 <1342303258@qq.com>
2018-12-24 21:54:03 +0800

    Weather-info & rt-monitor & depot-terminal

--- src/pages/contents/depot.vue
+++ src/pages/contents/depot.vue
@@ -6,7 +6,7 @@
           <img src="../../assets/images/airport/a_monitor.svg"/>实时监控
         </el-header>
         <section class="video">
-
+          <rt-moniter :video="node.points[0]"></rt-moniter>
         </section>
       </el-col>
       <el-col class="other">
@@ -14,25 +14,7 @@
           <el-header class="header font-24">
             <img src="../../assets/images/airport/a_weather.svg"/>室外天气
           </el-header>
-          <section class="w-info">
-            <i class="status d-b el-icon-success"></i>
-            <div class="items d-f">
-              <p>天气良好</p>
-              <p>晴天</p>
-              <p>风速 19km/h</p>
-            </div>
-            <div class="items d-f">
-              <p>可以起飞</p>
-              <p>气温 29℃</p>
-              <p>湿度 52.9%</p>
-            </div>
-            <div class="chart">
-              <p>未来1小时内不会有雨</p>
-              <div class="chart-view">
-                统计图...
-              </div>
-            </div>
-          </section>
+          <weather :weather="$store.state.weaInfo"></weather>
         </section>
         <section class="infos">
           <el-header class="header font-24">
@@ -41,7 +23,7 @@
           <table class="info-tab">
             <tr>
               <td class="id-num">编号</td>
-              <td>A009</td>
+              <td>{{ node.id }}</td>
               <td>
                 <el-button class="font-16" type="primary">查看历史监控</el-button>
               </td>
@@ -53,156 +35,14 @@
         </section>
       </el-col>
     </el-row>
-    <section class="control">
-      <el-header class="header font-24">
-        <img src="../../assets/images/airport/a_control.svg"/>控制
-      </el-header>
-      <el-row>
-        <el-col :span="12" class="control-left">
-          <el-col :span="5" class="battery text-c">
-            <img src="../../assets/images/airport/a_battery.svg"/>
-            <p>A仓电池</p>
-          </el-col>
-          <el-col :span="19" class="infos" tag="ul">
-            <li class="d-f">
-              <div class="item-label">循环次数</div>
-              <div class="item-text">
-                <span>100</span>
-                <span>状态正常</span>
-              </div>
-            </li>
-            <li class="d-f">
-              <div class="item-label">状态</div>
-              <div class="item-text status">
-                <el-progress :text-inside="true" :stroke-width="20" :percentage="100" status="success"></el-progress>
-              </div>
-            </li>
-            <li class="d-f">
-              <div class="item-label">维护</div>
-              <div class="item-text pos-r">
-                <img class="pos-a" src="../../assets/images/airport/a_maintain.svg"/>
-                <el-button class="font-16" type="danger">放入电池</el-button>
-                <el-button class="font-16" type="danger">取下电池</el-button>
-              </div>
-            </li>
-            <li class="d-f">
-              <div class="item-label">调试命令</div>
-              <div class="item-text debug pos-r">
-                <img class="pos-a" src="../../assets/images/airport/a_charging.svg"/>
-                <el-button class="font-16" type="warning">开始充电</el-button>
-                <el-button class="font-16" type="warning">停止充电</el-button>
-              </div>
-            </li>
-          </el-col>
-        </el-col>
-        <el-col :span="12">
-          <el-col :span="5" class="battery text-c">
-            <img src="../../assets/images/airport/a_battery.svg"/>
-            <p>B仓电池</p>
-          </el-col>
-          <el-col :span="19" class="infos" tag="ul">
-            <li class="d-f">
-              <div class="item-label">循环次数</div>
-              <div class="item-text">
-                <span>100</span>
-                <span>状态正常</span>
-              </div>
-            </li>
-            <li class="d-f">
-              <div class="item-label">状态</div>
-              <div class="item-text status">
-                <el-progress :text-inside="true" :stroke-width="20" :percentage="100" status="success"></el-progress>
-              </div>
-            </li>
-            <li class="d-f">
-              <div class="item-label">维护</div>
-              <div class="item-text pos-r">
-                <img class="pos-a" src="../../assets/images/airport/a_maintain.svg"/>
-                <el-button class="font-16" type="danger">放入电池</el-button>
-                <el-button class="font-16" type="danger">取下电池</el-button>
-              </div>
-            </li>
-            <li class="d-f">
-              <div class="item-label">调试命令</div>
-              <div class="item-text debug pos-r">
-                <img class="pos-a" src="../../assets/images/airport/a_charging.svg"/>
-                <el-button class="font-16" type="warning">开始充电</el-button>
-                <el-button class="font-16" type="warning">停止充电</el-button>
-              </div>
-            </li>
-          </el-col>
-        </el-col>
-      </el-row>
-    </section>
-    <section class="control high">
-      <el-header class="header font-24">
-        <img src="../../assets/images/airport/a_h_control.svg"/>高级控制
-      </el-header>
-      <el-row type="flex" class="high-content">
-        <el-col tag="ul" class="status">
-          <li class="font-18">状态：</li>
-          <li>
-            <img src="../../assets/images/airport/a_airport.svg"/>
-            <el-button class="font-16" type="primary">机场状态</el-button>
-          </li>
-          <li>
-            <img src="../../assets/images/airport/a_drone.svg"/>
-            <el-button class="font-16" type="primary">无人机状态</el-button>
-          </li>
-        </el-col>
-        <el-col tag="table" class="operate">
-          <tr>
-            <td class="font-18">电机运动</td>
-            <td class="text-c"><img src="../../assets/images/airport/a_airport.svg"/></td>
-            <td class="text-c"><img src="../../assets/images/airport/a_charge.svg"/></td>
-            <td class="text-c"><img src="../../assets/images/airport/a_fixed.svg"/></td>
-            <td class="text-c"><img src="../../assets/images/airport/a_get.svg"/></td>
-            <td class="text-c"><img src="../../assets/images/airport/a_reset.svg"/></td>
-          </tr>
-          <tr class="btns">
-            <td><el-button class="d-b font-16" type="danger">==急停==</el-button></td>
-            <td>
-              <el-button class="d-b font-16" type="warning">打开舱门</el-button>
-              <el-button class="d-b font-16" type="warning">关闭舱门</el-button>
-            </td>
-            <td>
-              <el-button class="d-b font-16" type="warning">开始充电</el-button>
-              <el-button class="d-b font-16" type="warning">停止充电</el-button>
-            </td>
-            <td>
-              <el-button class="d-b font-16" type="warning">固定无人机</el-button>
-              <el-button class="d-b font-16" type="warning">松开无人机</el-button>
-            </td>
-            <td>
-              <el-button class="d-b font-16" type="warning">取下飞机电池</el-button>
-              <el-button class="d-b font-16" type="warning">安装飞机电池</el-button>
-            </td>
-            <td><el-button class="d-b font-16" type="warning">复位</el-button></td>
-          </tr>
-        </el-col>
-      </el-row>
-      <section class="logs">
-        <header class="header font-18">日志：</header>
-        <ul class="logs-content">
-          <li>AUTO> Mode AUTO</li>
-        </ul>
-      </section>
-      <section class="debug">
-        <p>以下命令仅限开发人员调试使用</p>
-        <el-button class="font-16" type="danger">平台升顶</el-button>
-        <el-button class="font-16" type="danger">平台降底</el-button>
-        <el-button class="font-16" type="danger">平台举平</el-button>
-        <div class="btns f-r">
-          <el-input class="f-l inp"></el-input>
-          <el-button class="f-l font-16" type="danger">发送</el-button>
-          <el-button class="f-l font-16">清空</el-button>
-        </div>
-      </section>
-    </section>
+    <terminal :node="node"></terminal>
   </section>
 </template>
 
 <script>
+  import monitor from '../../components/rt-monitor/rt-monitor.vue'
+  import weather from '../../components/weather.vue'
+  import terminal from '../../components/depot_terminal/base.vue'
 
   export default {
     data() {
@@ -210,8 +50,20 @@
         activeIndex:'1'
       }
     },
-    components: {
+    props: {
+      node: {
+        type: Object,
+        required: true,
+        default: () => {}
+      }
+    },
+    mounted() {
 
+    },
+    components: {
+      'rt-moniter':monitor,
+      'weather':weather,
+      'terminal': terminal
     },
     methods: {
 
@@ -250,24 +102,12 @@
   }
   .infos {padding: 0 15px;}
   .video {
+    position: relative;
     width: 100%;
     height: 460px;
-    background-color: #eee;
+    background-color: #000;
   }
-  .weather .w-info .status {
-    color: #42a847;
-    font-size: 68px;
-    margin: 15px 0;
-  }
-  .weather .w-info .items {margin: 10px 0;}
-  .weather .w-info .items p {width: calc(100% / 3);}
-  .weather .w-info .chart {margin-top: 20px;}
-  .weather .w-info .chart-view {
-    width: 360px;
-    height: 120px;
-    margin: 15px 0;
-    background-color: #eee;
-  }
+
   .infos .info-tab{width: 100%;}
   .infos .info-tab td {
     white-space: nowrap;
