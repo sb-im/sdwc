c25293f7a581f5050714bbdbdc03b8161b72dc9d
lxb134230 <1342303258@qq.com>
2018-12-26 17:07:23 +0800

    Distinguish local and remote APIs

--- src/config/API.js
+++ src/config/API.js
@@ -1,6 +1,20 @@
 export default {
-  debug: '/user',
-  login: '/oauth/token',
-  nodes: '/api/v1/nodes',
-  plans: '/api/v1/plans'
+  // 服务器本地
+  local:{
+    debug: '/user',
+    login: '/oauth/token',
+    nodes: '/api/v1/nodes',
+    plans: '/api/v1/plans',
+  },
+  // 服务器远端
+  remote: {
+    // Reverse proxy corss GWF in China
+    gMap: "https://ditu.gdgdocs.org/maps/api/js",
+    // Google official map API
+    //gMap: "https://maps.googleapis.com/maps/api/js",
+    // Google China official map API
+    //gMap: "https://ditu.google.cn/maps/api/js",
+    cyApi: 'https://api.caiyunapp.com/v2',
+    weather: 'https://weather.sb.im/get'
+  }
 }
