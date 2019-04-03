8d54da7f956e285d3ae5262720725346b2ce0c15
Rocka <i@rocka.me>
2019-04-03 20:57:10 +0800

    feat(api): sdwc.config can be requested only once

--- src/api/sdwc.js
+++ src/api/sdwc.js
@@ -6,8 +6,10 @@ export function setBaseURL(url = '') {
   wr = wr.url(url.replace(/\/$/, ''), true);
 }
 
+let confProm;
 export function config() {
-  return wr.url('/config.json')
+  if (confProm) return confProm;
+  return confProm = wr.url('/config.json')
     .get()
     .json();
 }
