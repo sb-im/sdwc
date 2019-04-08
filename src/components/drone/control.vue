e536b9ceaa4b4fa54158d2448130342aed9350c9
Rocka <i@rocka.me>
2019-04-08 23:03:31 +0800

    feat(drone/control): preform `startmission_ready` check

--- src/components/drone/control.vue
+++ src/components/drone/control.vue
@@ -1,4 +1,5 @@
 <script>
+import MqttClient from '@/api/mqtt';
 import Control from '@/components/control.vue';
 
 const Controls = [
@@ -47,6 +48,57 @@ export default {
   computed: {
     controls() {
       return Controls;
+    }
+  },
+  methods: {
+    readyMission(name) {
+      return new Promise((resolve, reject) => {
+        if (!this.checkNodeStatus()) reject();
+        // msgbox's reference can't be stored
+        this.$msgbox({
+          type: 'info',
+          title: this.$t(name),
+          message: this.$t('common.operate_pending')
+        });
+        MqttClient.invoke(this.node.id, 'startmission_ready', [])
+          .then(() => {
+            // 'startmission_ready' returns 'OK'
+            this.$msgbox.close();
+            this.$confirm(this.$t('common.plan_ready'), {
+              type: 'warning',
+              title: this.$t(name)
+            }).then(() => {
+              // startmission when user confirm
+              resolve();
+            }).catch(() => {
+              reject();
+            });
+          })
+          .catch(e => {
+            // 'startmission_ready' failed
+            this.$msgbox.close();
+            const h = this.$createElement;
+            // message can be VNode
+            const message = h('div', [
+              h('p', this.$t('common.operate_error')),
+              h('p', `${e.code}: ${e.message}`)
+            ]);
+            this.$alert(message, {
+              type: 'error',
+              title: this.$t(name)
+            }).catch(() => { /* noop */ });
+            reject();
+          });
+      });
+    },
+    handleControl({ name, mission }) {
+      if (mission === 'readyMsission') {
+        this.readyMission(name).then(() => {
+          Control.methods.handleControl.apply(this, arguments);
+        }).catch(() => { /* noop */ });
+        return;
+      }
+      Control.methods.handleControl.apply(this, arguments);
     }
   }
 };
