f87410fdc0a267a604ac7ff220ab4e86a256ecd7
Rocka <i@rocka.me>
2019-05-15 18:58:06 +0800

    feat: map for drone

--- src/components/drone/drone.vue
+++ src/components/drone/drone.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="drone">
     <sd-node-drone-status :node="node" :message="message"></sd-node-drone-status>
+    <sd-map :path="mapPath"></sd-map>
     <sd-node-drone-control :node="node" :status="status"></sd-node-drone-control>
   </div>
 </template>
@@ -8,6 +9,7 @@
 <script>
 import Status from './status.vue';
 import Control from './control.vue';
+import NodeMap from '@/components/map/map.vue';
 
 export default {
   name: 'sd-node-drone',
@@ -24,14 +26,23 @@ export default {
       type: Array,
       required: true
     },
-    log : {
+    log: {
       type: Array,
       required: true
     }
   },
+  computed: {
+    mapPath() {
+      return this.message.map(msg => {
+        const { lat, lon: lng } = msg.status.gps;
+        return { lat, lng };
+      });
+    }
+  },
   components: {
     [Status.name]: Status,
-    [Control.name]: Control
+    [Control.name]: Control,
+    [NodeMap.name]: NodeMap
   }
 };
 </script>
