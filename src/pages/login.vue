219cd67aa117d71722e3d1ddd2db1d4cf6a5afda
Rocka <i@rocka.me>
2019-05-15 16:45:00 +0800

    refactor: login without vue-lbgv

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,42 +1,45 @@
 <template>
-  <login-bg :videos="video" image="/assets/images/login-backgound.jpg">
-    <template #header>
-      <el-row type="flex" justify="space-around">
-        <div class="login-bg">
-          <h1 class="login-title">S Dashboard Web Console</h1>
-          <el-form label-width="80px" label-position="left">
-            <el-form-item :label="$t('login.username')" :error="errorUsername">
-              <el-input type="text" v-model="username" autofocus></el-input>
-            </el-form-item>
-            <el-form-item :label="$t('login.password')" :error="errorPassword">
-              <el-input
-                ref="inputPwd"
-                type="password"
-                show-password
-                v-model="password"
-              ></el-input>
-            </el-form-item>
-            <el-button
-              type="primary"
-              icon="el-icon-minus"
-              @click="handleLogin"
-              :loading="pending"
-            >{{ $t('login.button') }}</el-button>
-          </el-form>
-        </div>
-      </el-row>
-    </template>
-  </login-bg>
+  <div class="login">
+    <div class="login__bg">
+      <img class="login__i" src="/assets/images/login-backgound.jpg" v-show="!showVideo">
+      <video
+        class="login__i"
+        :src="video"
+        muted
+        autoplay
+        loop
+        v-show="showVideo"
+        @canplay="showVideo = true"
+      ></video>
+    </div>
+    <div class="login__form">
+      <h1 class="login__title">S Dashboard Web Console</h1>
+      <el-form label-width="80px" label-position="left">
+        <el-form-item :label="$t('login.username')" :error="errorUsername">
+          <el-input type="text" v-model="username" autofocus></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('login.password')" :error="errorPassword">
+          <el-input ref="inputPwd" type="password" show-password v-model="password"></el-input>
+        </el-form-item>
+        <el-button
+          type="primary"
+          icon="el-icon-minus"
+          @click="handleLogin"
+          :loading="pending"
+        >{{ $t('login.button') }}</el-button>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
-import VueLbgv from 'vue-lbgv';
 
 export default {
   name: 'sd-login',
   data() {
     return {
+      showVideo: false,
       username: '',
       errorUsername: '',
       password: '',
@@ -46,7 +49,8 @@ export default {
   },
   computed: {
     video() {
-      return Array(7).fill(0).map((_, i) => `/assets/videos/aerial${i}-10s.mp4`);
+      const i = Math.floor(Math.random() * 7);
+      return `/assets/videos/aerial${i}-10s.mp4`;
     }
   },
   methods: {
@@ -85,24 +89,46 @@ export default {
         }
       });
     }
-  },
-  components: {
-    'login-bg': VueLbgv
   }
 };
 </script>
 
 <style>
-.login-title {
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.login__title {
   color: #409eff;
   margin: 0 0 20px;
   font-size: 26px;
+  text-align: center;
 }
 
-.login-bg {
+.login__form {
   width: 320px;
   height: 230px;
   padding: 36px;
   background: #fff9;
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.login__bg {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  position: fixed;
+}
+
+.login__i {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
 }
 </style>
