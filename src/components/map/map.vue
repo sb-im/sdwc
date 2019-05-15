6aaf81ecad1b4c5b1b6b607f0a472aeffa3f62b9
Rocka <i@rocka.me>
2019-05-15 18:57:40 +0800

    feat: add google map component

---
+++ src/components/map/map.vue
@@ -0,0 +1,26 @@
+<template>
+  <sd-card icon="drone/d_task" title="GPS" dense>
+    <sd-map-google v-bind="$attrs"></sd-map-google>
+  </sd-card>
+</template>
+
+<script>
+import Card from '@/components/card.vue';
+import Google from './google.vue';
+
+export default {
+  name: 'sd-map',
+  inheritAttrs: false,
+  components: {
+    [Card.name]: Card,
+    [Google.name]: Google
+  }
+};
+</script>
+
+<style>
+.map__el {
+  width: 100%;
+  height: 400px;
+}
+</style>
