e88e3a58245193330c0a0091dc172a7aa9586daa
Rocka <i@rocka.me>
2019-07-10 14:09:27 +0800

    build: move ssh-agent setup to before_script

--- scripts/gitlab-ci.sh
+++ scripts/gitlab-ci.sh
@@ -9,10 +9,3 @@ cp -r assets dist/
 mv dist/assets/robots.txt dist/
 mv dist/assets/favicon.ico dist/
 tar -cJf file.tar.xz -C dist/ .
-
-eval "$(ssh-agent -s)"
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-cp "$SSH_KNOWN_HOSTS" ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
