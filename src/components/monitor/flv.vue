8cdc5f2f27a71f19c25b318035b146604ece736b
Rocka <i@rocka.me>
2020-02-08 17:14:02 +0800

    fix(monitor): use prebulit flv and hls, fix transmuxing in worker

--- src/components/monitor/flv.vue
+++ src/components/monitor/flv.vue
@@ -13,7 +13,7 @@ export default {
   },
   methods: {
     loadPlayer() {
-      import(/* webpackChunkName: "flv" */ 'flv.js').then(({ default: flvjs }) => {
+      import(/* webpackChunkName: "flv" */ 'flv.js/dist/flv').then(({ default: flvjs }) => {
         if (flvjs.isSupported()) {
           this.player = flvjs.createPlayer(
             {
@@ -25,8 +25,7 @@ export default {
             },
             {
               enableWorker: true,
-              enableStashBuffer: false,
-              stashInitialSize: 128
+              enableStashBuffer: false
             }
           );
           this.player.attachMediaElement(this.$refs.video);
