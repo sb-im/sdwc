eb1019783ae3e83f5f9c9f8e70fbadab4a6d16f2
Rocka <i@rocka.me>
2020-04-18 13:55:40 +0800

    chore(i18n): remove unused date time formats

--- src/i18n/common.js
+++ src/i18n/common.js
@@ -1,9 +1,5 @@
 export const format = {
-  short: {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  },
+  // YYYY-MM-DD HH:mm
   long: {
     year: 'numeric',
     month: '2-digit',
@@ -12,16 +8,10 @@ export const format = {
     minute: '2-digit',
     hour12: false
   },
+  // HH:mm
   time: {
     hour: '2-digit',
     minute: '2-digit',
-    hour12: false
-  },
-  elapsed: {
-    timeZone: 'UTC',
-    hour: 'numeric',
-    minute: '2-digit',
-    second: '2-digit',
     hour12: false
   }
 };
