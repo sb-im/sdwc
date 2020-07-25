2ac3b198cdc0c07f8420687f8ed30a306ba0af37
Rocka <i@rocka.me>
2020-07-26 00:15:37 +0800

    feat(plan/new): show waypoints on map when select file

--- src/pages/plan/new.vue
+++ src/pages/plan/new.vue
@@ -1,25 +1,33 @@
 <template>
-  <sd-card icon="doc-add" title="plan.edit.add">
-    <template #action>
-      <el-button type="success" size="medium" icon="el-icon-document" @click="handleCreate">
-        <span v-t="'plan.edit.save'"></span>
-      </el-button>
-    </template>
-    <sd-plan-editable :initial="initial" ref="edit"></sd-plan-editable>
-  </sd-card>
+  <div>
+    <sd-card icon="doc-add" title="plan.edit.add">
+      <template #action>
+        <el-button type="success" size="medium" icon="el-icon-document" @click="handleCreate">
+          <span v-t="'plan.edit.save'"></span>
+        </el-button>
+      </template>
+      <sd-plan-editable ref="edit" :initial="initial" @waypoint-change="handleWaypointChange"></sd-plan-editable>
+    </sd-card>
+    <sd-map icon="map-waypoint" title="map.waypoint" fit v-bind="map"></sd-map>
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
 
 import Card from '@/components/card.vue';
+import SdMap from '@/components/map/map.vue';
 import PlanEditable from '@/components/plan/editable.vue';
 
 export default {
   name: 'sd-plan-new',
   data() {
     return {
-      initial: {}
+      initial: {},
+      map: {
+        path: [],
+        markers: []
+      }
     };
   },
   methods: {
@@ -31,10 +39,14 @@ export default {
       this.createPlan(plan)
         .then(p => this.$router.push({ name: 'plan/view', params: { id: p.id } }))
         .catch(() => this.$message.error(this.$t('plan.edit.create_failed')));
+    },
+    handleWaypointChange(map) {
+      this.map = map;
     }
   },
   components: {
     [Card.name]: Card,
+    [SdMap.name]: SdMap,
     [PlanEditable.name]: PlanEditable
   }
 };
