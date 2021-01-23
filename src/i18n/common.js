620a7dae77f7606eba6800340715cc366f6341bd
Rocka <i@rocka.me>
2021-01-23 12:03:16 +0800

    fix(i18n): hour format

--- src/i18n/common.js
+++ src/i18n/common.js
@@ -1,3 +1,4 @@
+/** @type {{ [key: string]: Intl.DateTimeFormatOptions }} */
 export const format = {
   // YYYY-MM-DD HH:mm
   long: {
@@ -6,19 +7,19 @@ export const format = {
     day: '2-digit',
     hour: '2-digit',
     minute: '2-digit',
-    hour12: false
+    hourCycle: 'h23'
   },
   // HH:mm
   time: {
     hour: '2-digit',
     minute: '2-digit',
-    hour12: false
+    hourCycle: 'h23'
   },
   // HH:mm:ss
   seconds: {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-    hour12: false
+    hourCycle: 'h23'
   }
 };
