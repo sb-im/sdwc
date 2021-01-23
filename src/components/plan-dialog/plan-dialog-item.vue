45ed9d3aeaf6df487857951541e3bd3b919315d1
Rocka <i@rocka.me>
2021-01-23 11:13:58 +0800

    feat(plan-dialog): add background color to warning/danger items

--- src/components/plan-dialog/plan-dialog-item.vue
+++ src/components/plan-dialog/plan-dialog-item.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="sd-plan-dialog__item">
+  <div class="sd-plan-dialog__item" :class="levelClass">
     <sd-icon v-if="icon" :value="icon" :size="30"></sd-icon>
     <div class="sd-plan-dialog__detail">
       <div class="sd-plan-dialog__title" v-t="title"></div>
       <slot></slot>
     </div>
-    <i class="sd-plan-dialog__icon" :class="iconClass"></i>
+    <div class="sd-plan-dialog__level">
+      <i class="sd-plan-dialog__icon" :class="iconClass"></i>
+    </div>
   </div>
 </template>
 
 <script>
 import Icon from '@/components/sd-icon.vue';
 import { getLevelIconClass } from '@/constants/level-icon-class';
+
+const LevelColorClass = {
+  warning: 'level--warning',
+  danger: 'level--danger'
+};
 
 export default {
   name: 'sd-plan-dialog-item',
@@ -28,6 +35,9 @@ export default {
     }
   },
   computed: {
+    levelClass() {
+      return LevelColorClass[this.level] || '';
+    },
     iconClass() {
       if (typeof this.level === 'string') return getLevelIconClass(this.level);
       return '';
@@ -41,12 +51,13 @@ export default {
 
 <style>
 .sd-plan-dialog__item {
-  margin: 10px 0;
+  margin: 6px 0;
+  padding: 2px 8px;
   display: flex;
+  border-radius: 4px;
 }
 .sd-plan-dialog__detail {
   flex-grow: 1;
-  padding: 0 8px;
   white-space: pre-wrap;
 }
 .sd-plan-dialog__title {
@@ -54,8 +65,19 @@ export default {
   line-height: 30px;
   font-weight: bold;
 }
+.sd-plan-dialog__level {
+  display: flex;
+  align-items: center;
+}
 .sd-plan-dialog__icon {
-  line-height: 30px;
   font-size: 24px;
+}
+.sd-plan-dialog__item.level--warning {
+  color: #e6a23c;
+  background-color: #fdf6ec;
+}
+.sd-plan-dialog__item.level--danger {
+  color: #f56c6c;
+  background-color: #fef0f0;
 }
 </style>
