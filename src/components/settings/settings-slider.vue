9dfa16e7f52e8e87a5f4cee69c984fcb084ef35a
Rocka <i@rocka.me>
2021-07-21 23:32:02 +0800

    feat(settings/slider): show range and current value

--- src/components/settings/settings-slider.vue
+++ src/components/settings/settings-slider.vue
@@ -5,6 +5,10 @@
     :min="range[0]"
     :max="range[1]"
     :step="step"
+    :marks="marks"
+    :show-input="true"
+    :show-input-controls="false"
+    :input-size="elFormItem.elFormItemSize"
     v-model="model"
     @change="handleChange"
   ></el-slider>
@@ -38,6 +42,16 @@ export default {
   data: () => ({
     model: null
   }),
+  inject: {
+    elFormItem: {
+      default: {}
+    }
+  },
+  computed: {
+    marks() {
+      return Object.fromEntries(this.range.map(v => [v, `${v}`]));
+    }
+  },
   methods: {
     handleChange(val) {
       this.$emit('change', val);
@@ -56,6 +70,58 @@ export default {
 
 <style>
 .settings__slider {
-  width: 160px;
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  flex-direction: row-reverse;
+}
+.settings__slider::before,
+.settings__slider::after {
+  content: none;
+  display: none;
+}
+.settings__slider .el-slider__runway {
+  flex-grow: 1;
+  margin: 0;
+}
+.settings__slider .el-slider__button-wrapper {
+  top: -9px;
+  width: 24px;
+  height: 24px;
+}
+.settings__slider .el-slider__button {
+  width: 12px;
+  height: 12px;
+}
+.settings__slider .el-slider__marks {
+  top: unset;
+  left: unset;
+  width: 100%;
+  line-height: 14px;
+}
+.settings__slider .el-slider__marks-stop {
+  display: none;
+}
+.settings__slider .el-slider__marks-text {
+  font-size: 12px;
+  margin-top: 0;
+  top: 8px;
+  -webkit-transform: none;
+  transform: none;
+}
+.settings__slider .el-slider__marks-text:last-child {
+  right: 0 !important;
+  left: unset !important;
+}
+.settings__slider .el-input-number {
+  float: none;
+  position: static;
+  display: block;
+  margin-top: 0;
+  margin-left: 6px;
+  width: 50px;
+}
+.settings__slider .el-input__inner {
+  padding: 0 !important;
 }
 </style>
