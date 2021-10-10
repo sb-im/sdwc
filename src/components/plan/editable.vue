ffc2dd3f7e2c6ea46fa739c90692d2b9f06d235b
Rocka <i@rocka.me>
2021-10-10 20:48:33 +0800

    fix(plan/editable): set drone id correctly

--- src/components/plan/editable.vue
+++ src/components/plan/editable.vue
@@ -16,7 +16,7 @@
     <el-form-item>
       <span slot="label" v-t="'plan.air'"></span>
       <el-select v-model="plan.node_id" :placeholder="$t('plan.edit.air_inp')">
-        <el-option v-for="d in drones" :key="d.id" :label="d.info.name" :value="d.info.id"></el-option>
+        <el-option v-for="d in drones" :key="d.info.id" :label="d.info.name" :value="d.info.id"></el-option>
       </el-select>
     </el-form-item>
     <el-form-item size="small">
