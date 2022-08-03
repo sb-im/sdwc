7a57f1d5aa081988253ee86cc58e39c31748f796
Rocka <i@rocka.me>
2022-08-03 18:21:44 +0800

    fix(status/depot): fix X axis label order

--- src/components/status/depot-status.vue
+++ src/components/status/depot-status.vue
@@ -137,7 +137,7 @@ export default {
       const now = Date.now();
       const { timestamp = 0, intervalsl = 0 } = this.charge.history_info;
       if (this.chargerInfoInterval > 0 || now < (timestamp + intervalsl) * 1000) return;
-      // 立即更新
+      // update chart data immediately on schedule
       this.updateChargerInfo().then(info => {
         const { timestamp, intervalsl } = info.history_info;
         const timeout = (timestamp + intervalsl + 1) * 1000 - now;
@@ -148,22 +148,29 @@ export default {
         }, timeout);
       });
     },
+    /**
+     * @param {number} offset number of intervals from timestamp
+     * @returns {string} time string in `hh:mm:ss`
+     */
     formatTime(offset) {
       const { timestamp, intervalsl } = this.charge.history_info;
       const dt = new Date((timestamp - intervalsl * offset) * 1000);
-      return dt.toLocaleString('zh', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, });
+      return this.$d(dt, 'seconds');
     },
     updateChart() {
+      /** @type { (h: { V: number, A: number }) => number } */
       const datum = this.popover.type === 'voltage' ? (h => h.V)
         : this.popover.type === 'current' ? (h => h.A)
           : (h => h.V * h.A);
-      /** @type {Chartist.IChartistData & { series: { x: number; y: number }[][] }} */
+      const history = this.charge.history;
+      const size = history.length;
+      /** @type {Chartist.IChartistData & { series: { value: number; meta: string }[][] }} */
       const data = { series: [[]] };
-      for (let i = this.charge.history.length - 1; i >= 0; i--) {
-        const h = this.charge.history[i];
-        data.series[0].push({ x: i, y: datum(h) });
+      for (let i = size - 1; i >= 0; i--) {
+        // meta's value shows on tooltip
+        data.series[0].push({ value: datum(history[i]), meta: this.formatTime(i) });
       }
-      const size = data.series[0].length;
+      // show at most 4 labels on X axis
       const step = size <= 4 ? 1 : Math.ceil(size / 4);
       /** @type {Chartist.ILineChartOptions} */
       const options = {
@@ -172,7 +179,7 @@ export default {
         showArea: true,
         axisX: {
           labelInterpolationFnc: (value, index) => {
-            return (index % step === 0) ? this.formatTime(index) : null;
+            return (index % step === 0) ? this.formatTime(size - index - 1) : '';
           }
         },
         axisY: { low: 0 },
@@ -181,8 +188,7 @@ export default {
             anchorToPoint: true,
             tooltipOffset: { x: 0, y: -14 },
             tooltipFnc: (meta, value) => {
-              const [offset, num] = value.split(',');
-              return `${this.formatTime(Number.parseInt(offset))} , ${Number.parseFloat(num).toFixed(2)}`;
+              return `${meta} , ${Number.parseFloat(value).toFixed(2)}`;
             }
           })
         ]
