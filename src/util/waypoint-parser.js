86489a33ffd45a44beab3f9e47b02983d930ad67
Rocka <i@rocka.me>
2023-05-19 16:05:26 +0800

    fix: kmz parsing with multiple wayline Folders

--- src/util/waypoint-parser.js
+++ src/util/waypoint-parser.js
@@ -157,11 +157,11 @@ export async function parseKMZ(buf) {
   // read template
   const template = zip['wpmz/template.kml'];
   const templateStr = sharedTextDecoder.decode(template);
-  const kml = sharedDOMParser.parseFromString(templateStr, 'text/xml');
-  const kmlFolder = kml.querySelector('Document>Folder');
-  const templateType = kmlFolder.getElementsByTagName('wpml:templateType')[0].textContent;
+  const templateXml = sharedDOMParser.parseFromString(templateStr, 'text/xml');
+  const templateFolder = templateXml.querySelector('Document>Folder');
+  const templateType = templateFolder.getElementsByTagName('wpml:templateType')[0].textContent;
   if (templateType === 'mapping2d') {
-    kmlFolder.querySelector('Placemark>Polygon coordinates')
+    templateFolder.querySelector('Placemark>Polygon coordinates')
       .textContent
       .replace(/[\n\t ]/g, '')
       .replace(/,0$/, '')
@@ -173,29 +173,36 @@ export async function parseKMZ(buf) {
   // read waypoint and actions
   const waylines = zip['wpmz/waylines.wpml'];
   const waylineStr = sharedTextDecoder.decode(waylines);
-  const xml = sharedDOMParser.parseFromString(waylineStr, 'text/xml');
-  const placemarks = xml.querySelectorAll('Document>Folder>Placemark');
-  const placemarkArray = Array.from(placemarks).sort((a, b) => {
-    const [ia, ib] = [a, b].map(e => Number.parseInt(e.getElementsByTagName('wpml:index')[0].textContent, 10));
+  const waylineXml = sharedDOMParser.parseFromString(waylineStr, 'text/xml');
+  const waylinesFolders = Array.from(waylineXml.querySelectorAll('Document>Folder'));
+  waylinesFolders.sort((a, b) => {
+    const [ia, ib] = [a, b].map(e => Number.parseInt(e.getElementsByTagName('wpml:waylineId')[0].textContent, 10));
     return ia - ib;
   });
-  for (const pm of placemarkArray) {
-    const [lng, lat /*, alt */] = pm.querySelector('Point>coordinates').textContent.split(',').map(Number.parseFloat);
-    const position = { lng, lat };
-    path.push(position);
-    const actionGroup = pm.getElementsByTagName('wpml:actionGroup')[0];
-    if (!actionGroup) continue;
-    const action = makeAction(`a${actions.length}`, position);
-    for (const act of Array.from(actionGroup.getElementsByTagName('wpml:action'))) {
-      const func = act.getElementsByTagName('wpml:actionActuatorFunc')[0].textContent;
-      const a = KMZActions[func];
-      if (!a) continue;
-      if (!action.action.includes(a)) {
-        action.action.push(a);
+  for (const folder of waylinesFolders) {
+    const placemarks = folder.getElementsByTagName('Placemark');
+    const placemarkArray = Array.from(placemarks).sort((a, b) => {
+      const [ia, ib] = [a, b].map(e => Number.parseInt(e.getElementsByTagName('wpml:index')[0].textContent, 10));
+      return ia - ib;
+    });
+    for (const pm of placemarkArray) {
+      const [lng, lat /*, alt */] = pm.querySelector('Point>coordinates').textContent.split(',').map(Number.parseFloat);
+      const position = { lng, lat };
+      path.push(position);
+      const actionGroup = pm.getElementsByTagName('wpml:actionGroup')[0];
+      if (!actionGroup) continue;
+      const action = makeAction(`a${actions.length}`, position);
+      for (const act of Array.from(actionGroup.getElementsByTagName('wpml:action'))) {
+        const func = act.getElementsByTagName('wpml:actionActuatorFunc')[0].textContent;
+        const a = KMZActions[func];
+        if (!a) continue;
+        if (!action.action.includes(a)) {
+          action.action.push(a);
+        }
       }
-    }
-    if (action.action.length > 0) {
-      actions.push(action);
+      if (action.action.length > 0) {
+        actions.push(action);
+      }
     }
   }
   prependHomeMark(path, actions);
